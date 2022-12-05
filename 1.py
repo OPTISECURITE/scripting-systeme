6917d23a88a18103ed015d486ca5df3526499c3b
OPTISECURITE <117072326+OPTISECURITE@users.noreply.github.com>
2022-12-05 13:52:45 +0100

    t

---
+++ 1.py
@@ -0,0 +1,41 @@
+# CTRL + K / C
+# CTRL + K / U
+
+#a = 1
+#[print(i) for i in range(10)]
+
+# for i in range (10):
+#     a+=1
+#     print(a)
+
+# elements = ["a","b","c"]
+
+# for element in elements:
+#     print("Ma lettre : {}".format(element))
+
+# a = ["a","b","c"]
+# b = 1
+
+# for number,i in enumerate(a) :
+#     print(f"{number}. Ma lettre : {i} ")
+
+# def maFonction():
+#     print("Hello World")
+#     return "mon retour"
+
+# print(maFonction)
+
+# def mafonction(texte, nombre):
+#     return texte * nombre
+# print(mafonction("Hello World   ", 5))
+
+import os
+import sys
+
+liste = os.listdir("C:\\Users")
+
+for nb,i in enumerate(liste):
+    print(f"{nb}. {i}")
+print(liste)
+
+print (sys.argv[0])
